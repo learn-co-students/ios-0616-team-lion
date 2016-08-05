8593203cd850745d2f5094a1ac6f2af85d5a802d
eldooon <eldonchan91@gmail.com>
2016-08-05 18:07:47 -0400

    completed intial collection layout & custom cell

--- TeamLionGroupProject/MarketplaceCollectionViewController.swift
+++ TeamLionGroupProject/MarketplaceCollectionViewController.swift
@@ -8,6 +8,52 @@
 
 import UIKit
 
-class MarketplaceCollectionViewController: UICollectionViewController {
-
+class MarketplaceCollectionViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //setup Layout
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.itemSize = CGSize(width: 300, height: 300)
+        
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "basicCell")
+        collectionView.backgroundColor = UIColor.blueColor()
+        self.view.addSubview(collectionView)
+        
+        
+        collectionView?.registerClass(PostViewCell.self, forCellWithReuseIdentifier: "basicCell")
+        
+        print("view LOADED")
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("basicCell", forIndexPath: indexPath) as! PostViewCell
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+    let layout = UICollectionViewFlowLayout()
+    
+    
+    
 }
--- TeamLionGroupProject/PostViewCell.swift
+++ TeamLionGroupProject/PostViewCell.swift
@@ -7,7 +7,67 @@
 //
 
 import UIKit
+import SnapKit
 
 class PostViewCell: UICollectionViewCell {
-
+    
+    var postImage = UIImageView()
+    var profileImage = UIImageView()
+    var nameLabel = UILabel()
+    var priceLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.backgroundColor = UIColor.redColor()
+        
+        //post Image contraints
+        contentView.addSubview(postImage)
+        postImage.image = UIImage(named: "Post")
+        postImage.snp_makeConstraints { (make) in
+            make.centerX.equalTo(contentView.snp_centerX)
+            make.centerY.equalTo(contentView.snp_centerY)
+            make.width.equalTo(contentView.snp_width)
+            make.height.equalTo(contentView.snp_height)
+        }
+        
+        //profile picture constraints
+        
+        contentView.addSubview(profileImage)
+        profileImage.image = UIImage(named: "Profile")
+        profileImage.image?.circle
+        profileImage.snp_makeConstraints { (make) in
+            make.centerX.equalTo(postImage.snp_centerX).offset(-110)
+            make.centerY.equalTo(postImage.snp_centerY).offset(-110)
+            make.width.equalTo(postImage.snp_width).dividedBy(5)
+            make.height.equalTo(postImage.snp_height).dividedBy(5)
+        }
+        
+        //name label constraints
+        contentView.addSubview(nameLabel)
+        nameLabel.text = "Name Label"
+        nameLabel.textColor = UIColor.whiteColor()
+        nameLabel.snp_makeConstraints { (make) in
+            make.centerX.equalTo(profileImage.snp_centerY).offset(160)
+            make.centerY.equalTo(profileImage.snp_centerY)
+            make.width.equalTo(profileImage.snp_width).multipliedBy(4)
+            make.height.equalTo(profileImage.snp_height)
+        }
+        
+        //price label constraints
+        contentView.addSubview(priceLabel)
+        priceLabel.text = "$$$"
+        priceLabel.textColor = UIColor.whiteColor()
+        priceLabel.snp_makeConstraints { (make) in
+            make.centerX.equalTo(contentView.snp_centerX).offset(120)
+            make.centerY.equalTo(contentView.snp_centerY).offset(120)
+            make.width.equalTo(contentView.snp_width).dividedBy(5)
+            make.height.equalTo(contentView.snp_height).dividedBy(5)
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
