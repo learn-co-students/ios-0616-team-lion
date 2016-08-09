d2dda6f6fdf83de6d7d4a59e17cd5b8339577176
davidvypark <davidvypark@gmail.com>
2016-08-09 14:30:46 -0400

    figured out segmented control in separate vc

--- TeamLionGroupProject/SegementPractice.swift
+++ TeamLionGroupProject/SegementPractice.swift
@@ -10,6 +10,48 @@ import UIKit
 
 class SegementPractice: UIViewController {
 	
+	override func loadView() {
+		super.loadView()
+		self.view.backgroundColor = UIColor.purpleColor()
+		print("view did load")
+		
+		//Initialize
+		let items = ["Purple", "Green", "Blue"]
+		let customSC = UISegmentedControl(items: items)
+		customSC.selectedSegmentIndex = 0
+		
+		//Set up Frame and Segmented Control
+		let frame = UIScreen.mainScreen().bounds
+		customSC.frame = CGRectMake(frame.minX + 10, frame.minY + 50, frame.width - 20, frame.height*0.1)
+		
+		//Style the Segmented Control
+		customSC.layer.cornerRadius = 5
+		customSC.backgroundColor = UIColor.blackColor()
+		customSC.tintColor = UIColor.whiteColor()
 	
+		//Add target action method
+		customSC.addTarget(self, action: #selector(changeColor), forControlEvents: .ValueChanged)
+		
+		//Add SegmentedControl to view
+		view.addSubview(customSC)
+	}
+	
+	func changeColor(sender: UISegmentedControl) {
+		switch sender.selectedSegmentIndex {
+		case 1:
+			view.backgroundColor = UIColor.greenColor()
+		case 2:
+			view.backgroundColor = UIColor.blueColor()
+		default:
+			view.backgroundColor = UIColor.purpleColor()
+		}
+	}
+	override func viewDidLoad() {
+		 super.viewDidLoad()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
 
 }
