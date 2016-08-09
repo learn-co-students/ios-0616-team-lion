95eafd7b7a7665e7ad6ac73f7bc34756c86b1c4b
davidvypark <davidvypark@gmail.com>
2016-08-09 11:07:12 -0400

    completed basic layout of Profile header and profile VC

--- TeamLionGroupProject/ProfileViewController.swift
+++ TeamLionGroupProject/ProfileViewController.swift
@@ -13,13 +13,12 @@ class ProfileViewController: UIViewController {
 	private let cellIdentifier = "Cell"
 	private let headerIdentifier = "header"
 	
-//	required init?(coder aDecoder: NSCoder) {
-//		fatalError("init(coder:) has not been implemented")
-//	}
+	var collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = UIColor.emeraldColor()
+		
+		setupCollectionView()
 		
 	}
 	
@@ -28,6 +27,23 @@ class ProfileViewController: UIViewController {
 		let layout = UICollectionViewFlowLayout()
 		layout.minimumLineSpacing = 1
 		layout.minimumInteritemSpacing = 1
+		let blockDimension = (view.frame.width - 2)/3
+		layout.itemSize = CGSize(width: blockDimension, height: blockDimension)
+		layout.headerReferenceSize = CGSizeMake(0, CGRectGetHeight(view.frame)/3)
+		
+		collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+		collectionView.backgroundColor = UIColor.darkGrayColor()
+		collectionView.dataSource = self
+		collectionView.delegate = self
+		
+		collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
+		collectionView.registerClass(ProfileHeaderView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerIdentifier)
+		
+		view.addSubview(collectionView)
+		
+		collectionView.snp_makeConstraints { (make) in
+			make.edges.equalTo(view.snp_edges)
+		}
 	}
 
 }
@@ -36,13 +52,17 @@ class ProfileViewController: UIViewController {
 
 extension ProfileViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
 	
+	func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+		return 1
+	}
+	
 	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 3
+		return 20
 	}
 	
 	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath)
-		cell.backgroundColor = UIColor.belizeHoleColor()
+		cell.backgroundColor = UIColor.peterRiverColor()
 		
 		return cell
 	}
