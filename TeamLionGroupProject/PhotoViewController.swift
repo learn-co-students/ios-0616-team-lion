914b1ab07c5f163237a8bfa86fed882abbb6edf3
eldooon <eldonchan91@gmail.com>
2016-08-09 18:01:56 -0400

    added initial layout for camera view controller

--- TeamLionGroupProject/PhotoViewController.swift
+++ TeamLionGroupProject/PhotoViewController.swift
@@ -9,45 +9,114 @@
 import UIKit
 import AVFoundation
 
-class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class PhotoViewController: UIViewController {
     
-    let captureSession = AVCaptureSession()
-    var captureDevice: AVCaptureDevice?
-    let cameraImageView = UIImageView()
-    let photoLibraryButton = UIButton()
-
+    var previewView = UIView()
+    var capturedImage = UIImageView()
+    let captureButton = UIButton()
+    
+    var captureSession: AVCaptureSession?
+    var stillImageOutput: AVCaptureStillImageOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.whiteColor()
+        layOutPhoto()
         
-        view.backgroundColor = UIColor.amethystColor()
-        let devices = AVCaptureDevice.devices()
-        
-        setUpCameraLayout()
-    
     }
     
-    func setUpCameraLayout() {
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
-        self.view.addSubview(cameraImageView)
-        cameraImageView.snp_makeConstraints { (make) in
+        captureSession = AVCaptureSession()
+        captureSession!.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        let backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var error: NSError?
+        var input: AVCaptureDeviceInput!
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch let error1 as NSError {
+            error = error1
+            input = nil
+        }
+        
+        if error == nil && captureSession!.canAddInput(input) {
+            captureSession!.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput!.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+            if captureSession!.canAddOutput(stillImageOutput) {
+                captureSession!.addOutput(stillImageOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer!.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.Portrait
+                previewView.layer.addSublayer(previewLayer!)
+                
+                captureSession!.startRunning()
+            }
+        }
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer!.frame = previewView.bounds
+    }
+    
+    func didPressTakePhoto(sender: UIButton) {
+        
+        if let videoConnection = stillImageOutput!.connectionWithMediaType(AVMediaTypeVideo) {
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {(sampleBuffer, error) in
+                if (sampleBuffer != nil) {
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    let dataProvider = CGDataProviderCreateWithCFData(imageData)
+                    let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent.RenderingIntentDefault)
+                    
+                    let image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
+                    self.capturedImage.image = image
+                }
+            })
+        }
+    }
+    
+    func didPressTakeAnother(sender: AnyObject) {
+        captureSession!.startRunning()
+    }
+    
+    func layOutPhoto() {
+        
+        view.addSubview(previewView)
+        previewView.snp_makeConstraints { (make) in
             make.centerX.equalTo(view.snp_centerX)
             make.centerY.equalTo(view.snp_centerY)
             make.width.equalTo(view.snp_width)
-            make.height.equalTo(view.snp_height).dividedBy(2)
+            make.height.equalTo(view.snp_height)
         }
         
-        self.view.addSubview(photoLibraryButton)
-        photoLibraryButton.setTitle("Library", forState: .Normal)
-        photoLibraryButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        photoLibraryButton.snp_makeConstraints { (make) in
-            make.centerX.equalTo(view.snp_centerX).dividedBy(4.5)
-            make.centerY.equalTo(view.snp_centerY).dividedBy(4.5)
+        view.addSubview(capturedImage)
+        capturedImage.snp_makeConstraints { (make) in
+            make.centerX.equalTo(view.snp_centerX).dividedBy(2.8)
+            make.centerY.equalTo(view.snp_centerY).multipliedBy(1.6)
             make.width.equalTo(view.snp_width).dividedBy(5)
-            make.height.equalTo(view.snp_height).dividedBy(5)
+            make.height.equalTo(view.snp_width).dividedBy(5)
         }
+
+        view.addSubview(captureButton)
+        captureButton.setImage(UIImage(named: "circleButton" ), forState: .Normal)
+        captureButton.addTarget(self, action: #selector(PhotoViewController.didPressTakePhoto(_:)), forControlEvents: .TouchUpInside)
+        captureButton.snp_makeConstraints { (make) in
+            make.centerX.equalTo(view.snp_centerX)
+            make.centerY.equalTo(view.snp_centerY).multipliedBy(1.6)
+            make.width.equalTo(view.snp_width).dividedBy(3.7)
+            make.height.equalTo(view.snp_width).dividedBy(3.7)
+        }
+        
     }
     
-    
-
-
 }
