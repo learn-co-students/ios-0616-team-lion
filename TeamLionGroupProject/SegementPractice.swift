7a6c6e9bfda2085fdf89f228e123c64318ffd743
davidvypark <davidvypark@gmail.com>
2016-08-09 13:56:25 -0400

    created SegmentPractice VC to figure out Segmented Control

---
+++ TeamLionGroupProject/SegementPractice.swift
@@ -0,0 +1,15 @@
+//
+//  SegementPractice.swift
+//  TeamLionGroupProject
+//
+//  Created by David Park on 8/9/16.
+//  Copyright © 2016 TeamLion. All rights reserved.
+//
+
+import UIKit
+
+class SegementPractice: UIViewController {
+	
+	
+
+}
