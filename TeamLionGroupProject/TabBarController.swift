462a3fb3ffad5bc3f3be1b03a0aeda989197edf4
eldooon <eldonchan91@gmail.com>
2016-08-18 17:19:32 -0400

    added chat tab icon

--- TeamLionGroupProject/TabBarController.swift
+++ TeamLionGroupProject/TabBarController.swift
@@ -24,9 +24,8 @@ class TabBarController: UITabBarController {
         self.tabBar.tintColor = UIColor.clearColor()
 		
 		let openChat = OpenChatViewController()
-		openChat.tabBarItem = UITabBarItem(title:nil, image: UIImage(named: "profileUnselected")?.imageWithRenderingMode(.AlwaysOriginal), selectedImage: UIImage(named: "profileSelected")?.imageWithRenderingMode(.AlwaysOriginal))
+		openChat.tabBarItem = UITabBarItem(title:nil, image: UIImage(named: "chatUnselected")?.imageWithRenderingMode(.AlwaysOriginal), selectedImage: UIImage(named: "chatSelected")?.imageWithRenderingMode(.AlwaysOriginal))
 		openChat.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-		
 		
         let profile = ProfileViewController()
         //let profile = LoginViewController()
